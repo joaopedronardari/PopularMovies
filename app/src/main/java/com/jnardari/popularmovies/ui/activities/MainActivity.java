c45471433dc9ebd0d6a6b29a479e161669bdb052
Joao Pedro Nardari dos Santos <jnardari@ciandt.com>
2016-08-15 04:21:15 -0300

    Adding Packages

--- app/src/main/java/com/jnardari/popularmovies/ui/activities/MainActivity.java
+++ app/src/main/java/com/jnardari/popularmovies/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jnardari.popularmovies;
+package com.jnardari.popularmovies.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jnardari.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
